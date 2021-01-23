f43e24f152ecdb0ee8d7cc575a24c5c5c8c9ff6a
theart84 <artshadow74@gmail.com>
2021-01-23 17:47:52 +0500

    Change base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import {getItem} from "@/helpers/persistanceStorage";
 
-// axios.defaults.baseURL = 'https://wh-backend-rest-mks.herokuapp.com/api';
+axios.defaults.baseURL = 'https://wh-backend-rest-mks.herokuapp.com/api';
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
 axios.interceptors.request.use(config => {
   const token = getItem('accessToken');
   config.headers.Authorization = token ? token : '';
